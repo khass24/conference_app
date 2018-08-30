e4ad9e2b059640f85d5c6501772dbccfd32b9260
Kyle Hassett <kylehassett72@gmail.com>
2018-08-30 16:40:32 -0500

    Day 2 Conference App commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@ Speaker.create({
                 last_name: "Zaghian", 
                 email: "danielle@actualize.co"
                 })
+
+Meeting.create({
+                title: "Group naptime",
+                agenda: "Get some serious Z's",
+                time: "30 minutes"
+                })
