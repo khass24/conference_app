b10e5c830647b08007fbb6200fdf3a7dbd1101e8
Kyle Hassett <kylehassett72@gmail.com>
2018-09-06 14:33:21 -0500

    added validations to meeting and speaker models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,9 @@
 class Meeting < ApplicationRecord
+  validates :title, length: {minimum: 2}
+  validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
+  validates :time, numericality: {greather_than: 0}
 end
